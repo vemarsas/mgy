b039d050b478696161c0b27bdcd38e4dea6055c3
Guido De Rosa <guidoderosa@gmail.com>
2023-07-18 13:07:46 +0100

    one setup

--- etc/scripts/platform/debian/setup.sh
+++ etc/scripts/platform/debian/setup.sh
@@ -9,9 +9,6 @@ set +e
 # is copied / placed in the relevant directory with proper
 # ownership/permissions.
 # This script takes control from there.
-# Another script has been implemented for that very initial
-# bootstrap: it is located at top-level dir of this project
-# and it is named setup.sh as well (at the time of writing this note).
 
 # echo $* # DEBUG
 
@@ -55,7 +52,14 @@ cd $PROJECT_ROOT
 apt-get update
 apt-get -y upgrade
 
-apt-get -y install ruby ruby-dev ruby-rack ruby-rack-protection ruby-locale sudo iproute2 iptables bridge-utils pciutils usbutils usb-modeswitch dhcpcd5 dnsmasq resolvconf locales ifrename build-essential ca-certificates libssl-dev ntp psmisc
+apt-get -y install \
+    ruby ruby-dev ruby-rack ruby-rack-protection ruby-locale \
+    sudo locales psmisc \
+    iproute2 iptables bridge-utils dhcpcd5 dnsmasq resolvconf ifrename ntp \
+    openvpn hostapd \
+    pciutils usbutils usb-modeswitch \
+    build-essential \
+    ca-certificates libssl-dev 
 
 install_conffiles
 
